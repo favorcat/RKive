95042aa145081984b63c99887b11759ea9894382
favorcat.mac <flower_hana39@naver.com>
2021-05-01 21:54:13 +0900

    fix hashtag

--- src/components/nameBox.vue
+++ src/components/nameBox.vue
@@ -11,11 +11,11 @@
       <div class="dropdown-content">
         <!-- BTS twt -->
         <a @click="current('BTS', 'BTS_twt', '방탄소년단', 'ALL')">방탄소년단</a>
-        <a v-for="hash in bts_twt" :key="hash" class="hashtag" @click="current('BTS', 'BTS_twt', hash)"> {{ hash }} </a>
+        <a v-for="hash in bts_twt" :key="hash" class="hashtag" @click="current('BTS', 'BTS_twt', '방탄소년단', hash)"> {{ hash }} </a>
         <div class="divider"/>
         <!-- global official -->
         <a @click="current('BTS', 'bts_bighit', 'BTS official', 'ALL')">BTS official</a>
-        <a v-for="hash in bts_bighit" :key="hash" class="hashtag" @click="current('BTS', 'bts_bighit', hash)"> {{ hash }} </a>
+        <a v-for="hash in bts_bighit" :key="hash" class="hashtag" @click="current('BTS', 'bts_bighit', 'BTS official', hash)"> {{ hash }} </a>
         <div class="divider"/>
         <!-- 일본 official -->
         <a @click="current('BTS', 'BTS_jp_official', 'BTS JAPAN OFFICIAL', 'ALL')">BTS JAPAN OFFICIAL</a>
@@ -30,15 +30,15 @@
       <div class="dropdown-content">
         <!-- BIGHIT MUSIC -->
         <a @click="current('HYBE', 'BIGHIT_MUSIC', 'BIGHIT MUSIC', 'ALL')">BIGHIT MUSIC</a>
-        <a v-for="hash in bighitmusic" :key="hash" class="hashtag" @click="current('HYBE', 'BIGHIT_MUSIC', hash)"> {{ hash }} </a>
+        <a v-for="hash in bighitmusic" :key="hash" class="hashtag" @click="current('HYBE', 'BIGHIT_MUSIC', 'BIGHIT MUSIC', hash)"> {{ hash }} </a>
         <div class="divider"/>
         <!-- Weverse -->
         <a @click="current('HYBE', 'weverseofficial', 'Weverse', 'ALL')">Weverse</a>
-        <a v-for="hash in weverseofficial" :key="hash" class="hashtag" @click="current('HYBE', 'weverseofficial', hash)"> {{ hash }} </a>
+        <a v-for="hash in weverseofficial" :key="hash" class="hashtag" @click="current('HYBE', 'weverseofficial', 'Weverse', hash)"> {{ hash }} </a>
         <div class="divider"/>
         <!-- HYBE OFFICIAL -->
         <a @click="current('HYBE', 'HYBEOFFICIALtwt', 'HYBE OFFICIAL', 'ALL')">HYBE OFFICIAL</a>
-        <a v-for="hash in hybe" :key="hash" class="hashtag" @click="current('HYBE', 'HYBEOFFICIALtwt', hash)"> {{ hash }} </a>
+        <a v-for="hash in hybe" :key="hash" class="hashtag" @click="current('HYBE', 'HYBEOFFICIALtwt', 'HYBE OFFICIAL', hash)"> {{ hash }} </a>
         <div class="divider"/>
         <!-- HYBE Japan OFFICIAL -->
         <a @click="current('HYBE', 'HYBE_LABELS_JP', 'HYBE LABELS JAPAN', 'ALL')">HYBE LABELS JAPAN</a>
@@ -53,11 +53,11 @@
       <div class="dropdown-content">
         <!-- weverseshop -->
         <a @click="current('Merch', 'weverseshop', 'Weverse Shop', 'ALL')">Weverse Shop</a>
-        <a v-for="hash in shop" :key="hash" class="hashtag" @click="current('Merch', 'weverseshop', hash)"> {{ hash }} </a>
+        <a v-for="hash in shop" :key="hash" class="hashtag" @click="current('Merch', 'weverseshop', 'Weverse Shop', hash)"> {{ hash }} </a>
         <div class="divider"/>
         <!-- HYBE MERCH -->
         <a @click="current('Merch', 'HYBE_MERCH', 'HYBE MERCH', 'ALL')">HYBE MERCH</a>
-        <a v-for="hash in shop" :key="hash" class="hashtag" @click="current('Merch', 'HYBE_MERCH', hash)"> {{ hash }} </a>
+        <a v-for="hash in shop" :key="hash" class="hashtag" @click="current('Merch', 'HYBE_MERCH', 'HYBE MERCH', hash)"> {{ hash }} </a>
         <div class="divider"/>
         <!-- BigHitShop -->
         <a @click="current('Merch', 'BigHitShop', 'Big Hit official shop', 'ALL')">Big Hit official shop</a>
@@ -106,11 +106,11 @@
       <div class="dropdown-content">
         <!-- BTS WORLD Official -->
         <a @click="current('Game', 'BTSW_official', 'BTS WORLD Official', 'ALL')">BTS WORLD Official</a>
-        <a v-for="hash in btsw_official" :key="hash" class="hashtag" @click="current('Game', 'BTSW_official', hash)"> {{ hash }} </a>
+        <a v-for="hash in btsw_official" :key="hash" class="hashtag" @click="current('Game', 'BTSW_official', 'BTS WORLD Official', hash)"> {{ hash }} </a>
         <div class="divider"/>
         <!-- Rhythm Hive Official -->
         <a @click="current('Game', 'rhythmhive_twt', 'Rhythm Hive Official', 'ALL')">Rhythm Hive Official</a>
-        <a v-for="hash in rhythmhive_twt" :key="hash" class="hashtag" @click="current('Game', 'rhythmhive_twt', hash)"> {{ hash }} </a>
+        <a v-for="hash in rhythmhive_twt" :key="hash" class="hashtag" @click="current('Game', 'rhythmhive_twt', 'Rhythm Hive Official', hash)"> {{ hash }} </a>
       </div>
     </div>
   </div>
@@ -124,7 +124,7 @@ export default {
       bts_twt: ['#JIN', '#SUGA', '#RM', '#JHope', '#JIMIN', '#V', '#JK', '#김데일리', '#홉필름', '#우리아미상받았네'],
       bts_bighit: ['공지', '#오늘의방탄', '#방탄밤', 'Episode'],
 
-      weverseofficial: ['[위버스 매거진]'],
+      weverseofficial: ['위버스 매거진'],
       bighitmusic: ['방송', '기사', '위버스 매거진'],
       hybe: ['기사', '위버스 매거진', 'HYBE INSIGHT'],
 
